77f41b6f6afc64c67c969ba20e72291b98baf954
abcds07 <abhisheks.ug20.cse@nitp.ac.in>
2022-07-14 11:34:23 +0530

    My First Commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:samachar/pages/home.dart';
+import 'package:samachar/utils/colors.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          brightness: Brightness.dark, primaryColor: AppColors.primary),
+      home: Home(),
+    );
+  }
+}
